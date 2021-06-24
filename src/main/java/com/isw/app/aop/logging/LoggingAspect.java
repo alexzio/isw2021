f53cddc0ff1d9170a8cf89204d4044217ddfe580
Alexzio Figueroa <68154494+alexzio@users.noreply.github.com>
2021-06-24 10:45:00 -0300

    chuchi

--- src/main/java/com/isw/app/aop/logging/LoggingAspect.java
+++ src/main/java/com/isw/app/aop/logging/LoggingAspect.java
@@ -16,7 +16,7 @@ import tech.jhipster.config.JHipsterConstants;
 /**
  * Aspect for logging execution of service and repository Spring components.
  *
- * miau miau miau
+ * miau miau miau oink oink
  */
 @Aspect
 public class LoggingAspect {
